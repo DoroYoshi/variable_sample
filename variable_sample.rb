06841c46392fc4c32567ca5587e9d1cd74ef7489
DoroYoshi <yoshimatsu44dy@gmail.com>
2019-12-09 22:31:09 +0900

    完成。

---
+++ variable_sample.rb
@@ -0,0 +1,13 @@
+name = "Dorokawa"
+age = 30
+address = "Aichi"
+tell = "080-2344-xxxx"
+
+puts <<~TEXT
+【自己紹介】
+私の名前は#{name}です。
+年齢は#{age}です。
+今住んでいるところは#{address}です。
+電話番号は#{tell}です。
+宜しくお願い致します！
+TEXT
